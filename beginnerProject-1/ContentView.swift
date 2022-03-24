a0c5d1f69995ba077c9d6ba16280da84f30d265d
wezleyp <wesley.patterson121@gmail.com>
2022-03-23 22:20:27 -0500

    Initial Commit

---
+++ beginnerProject-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  beginnerProject-1
+//
+//  Created by Wesley Patterson on 3/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ beginnerProject-1/beginnerProject_1App.swift
@@ -0,0 +1,17 @@
+//
+//  beginnerProject_1App.swift
+//  beginnerProject-1
+//
+//  Created by Wesley Patterson on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct beginnerProject_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
